bb46cb1c283f158f6668813ec27d4b9235bf30c6
Alexandre Pedrecal <pedrecalc@gmail.com>
2025-03-01 12:36:37 -0300

    refactor: rename DATABASE_URL to STORAGE_DATABASE_URL in configuration and environment files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.STORAGE_DATABASE_URL,
   },
   tablesFilter: ["app_gallery_*"],
 } satisfies Config;
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,7 +2,5 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-// TODO: Learn why did i have to disable these rules
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.STORAGE_DATABASE_URL!);
 export const db = drizzle(sql, { schema });
